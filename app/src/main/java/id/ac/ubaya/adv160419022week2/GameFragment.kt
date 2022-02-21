0413e96408b3083d27d483eb882bf56a6ffa6169
Augie <augie100801@gmail.com>
2022-02-21 16:47:55 +0700

    selesai homework week 2

--- app/src/main/java/id/ac/ubaya/adv160419022week2/GameFragment.kt
+++ app/src/main/java/id/ac/ubaya/adv160419022week2/GameFragment.kt
@@ -1,16 +1,21 @@
 package id.ac.ubaya.adv160419022week2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_game.*
-import kotlinx.android.synthetic.main.fragment_main.*
+import kotlin.random.Random
 
 
 class GameFragment : Fragment() {
+    var result:Int = 0
+    var answer:Int = 0
+    var score:Int = 0
+    var random = listOf<Int>()
 
 
     override fun onCreateView(
@@ -27,11 +32,40 @@ class GameFragment : Fragment() {
             val playerName = GameFragmentArgs.fromBundle(requireArguments()).playerName
             txtTurn.text = "$playerName's Turn"
         }
-        btnBack.setOnClickListener {
-//            Dapatkan action yg ingin dijalankan
-            val action = GameFragmentDirections.actionMainFragment()
-//            Jalankan action tsb
-            Navigation.findNavController(it).navigate(action)
+
+        score = 0
+        GetRandom()
+
+
+        btnSubmit.setOnClickListener {
+            answer = inputAnswer.text.toString().toInt()
+            Log.d("Answer", answer.toString())
+            if (answer == result)
+            {
+                score++
+                GetRandom()
+                inputAnswer.getText()?.clear()
+            }
+            else
+            {
+                // Dapatkan action yg ingin dijalankan
+                val action = GameFragmentDirections.actionResultFragment("GAME OVER", score)
+                //Jalankan action tsb
+                Navigation.findNavController(it).navigate(action)
+            }
         }
+    }
+
+    fun GetRandom(){
+        val randomValues = List(2) { Random.nextInt(1, 99) }
+
+        // RANDOM NUMBER
+        result = randomValues[0]+randomValues[1]
+        Log.d("Values", randomValues.toString())
+        Log.d("Result", result.toString())
+
+        // SHOW RANDOM
+        decimalOne.text = randomValues[0].toString()
+        decimalTwo.text = randomValues[1].toString()
     }
 }
---
+++ app/src/main/java/id/ac/ubaya/adv160419022week2/ResultFragment.kt
@@ -0,0 +1,37 @@
+package id.ac.ubaya.adv160419022week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_game.*
+import kotlinx.android.synthetic.main.fragment_result.*
+
+class ResultFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            val resultStatus = ResultFragmentArgs.fromBundle(requireArguments()).resultStatus
+            val score = ResultFragmentArgs.fromBundle(requireArguments()).score
+            txtResult.text = resultStatus
+            txtScore.text = score.toString()
+        }
+
+        btnBack.setOnClickListener {
+            val action = ResultFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
